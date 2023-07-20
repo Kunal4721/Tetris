4cc5e5c1d4b8312bcc5da0fb0bc77cb17875dc58
Kunal4721 <kunalbajaj20220@gmail.com>
2023-07-20 12:52:04 -0400

    Added some global variables corresponding to dimensions

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+
+# 10 width X 20 height square blocks
+
+#Some Global Variables
+screen_width = 800
+screen_height = 700
+
+play_width = 300 #10 blocks of 30 width
+play_height = 600 #20 blocks of 20 height
+
+block_size = 30
